dfa42568c35a24baa50ad030b9beed6aae5dd9f4
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-16 17:35:10 +0000

    gettting the system to run

--- src/users_access/serializers/user_settings/setting_serializer.py
+++ src/users_access/serializers/user_settings/setting_serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ..models import UserSetting
+from users_access.models.user_settings import UserSetting
 
 class UserSettingSerializer(serializers.ModelSerializer):
     class Meta:
