04145a179e7c1e9191dad8ba695adf654a21b3e0
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-16 13:57:37 +0000

    User access domain completed

--- src/users_access/urls.py
+++ src/users_access/urls.py
@@ -1,48 +1,43 @@
 from django.urls import path
-from .controllers.create import UserSettingsToggleAPI, Enable2FAAPIView
-from .controllers.details import UserSettingsListAPIView
-
-from django.urls import path
-from helpers import fields as input_fields
-from .controllers.avatar import UserAvatarAPI
-from .controllers.create import UserRegistrationAPI
-from .controllers.names_update import UserNamesUpdateAPI
-from .controllers.password_update import UserPasswordUpdateAPI
-from .controllers.user_account import UserAccountAPI
-from .controllers.login import UserLoginAPIView
-from .controllers.login_2fa import TwoFactorVerificationAPIView
-from .controllers.resend_2fa import ResendTwoFactorTokenAPIView
-from .controllers.forgot_password import (
+from .views.user_settings.create import UserSettingsToggleAPI, Enable2FAAPIView
+from .views.user_settings.details import UserSettingsListAPIView
+from .views.users.avatar import UserAvatarAPI
+from .views.users.create import UserRegistrationAPI
+from .views.users.names_update import UserNamesUpdateAPI
+from .views.users.password_update import UserPasswordUpdateAPI
+from .views.users.user_account import UserAccountAPI
+from .views.users.login import UserLoginAPIView
+from .views.users.login_2fa import TwoFactorVerificationAPIView
+from .views.users.resend_2fa import ResendTwoFactorTokenAPIView
+from .views.users.forgot_password import (
     SendForgotPasswordOTPAPIView,
     PasswordResetOTPVerificationAPIView,
     CreateNewPasswordTokenAPIView
 )
 
-from .controllers.logout import (
+from .views.users.logout import (
     LogoutViewAPI,
     LogoutAllViewAPI
 )
-from .controllers.user_status import UserStatusAPI
+from .views.users.user_status import UserStatusAPI
 
-app_name = "user"
+app_name = "user_access"
 
 urlpatterns = [
-    path("register/", UserRegistrationAPI.as_view(), name=input_fields.REGISTER),
-    path("login/", UserLoginAPIView.as_view(), name=input_fields.LOGIN),
+    path("register/", UserRegistrationAPI.as_view(), name="register"),
+    path("login/", UserLoginAPIView.as_view(), name="login"),
     path("login/verify/<str:endpoint_token>/", TwoFactorVerificationAPIView.as_view(), name='two-factor-verify'),
     path("resend-token/<str:endpoint_token>/", ResendTwoFactorTokenAPIView.as_view(), name='resent-two-factor-token'),
-    path("logout/", LogoutViewAPI.as_view(), name=input_fields.LOGOUT),
-    path("logoutall/", LogoutAllViewAPI.as_view(), name=input_fields.LOGOUTALL),
-    path("change-avatar/", UserAvatarAPI.as_view(), name=input_fields.CHANGE_AVATAR),
-    path("change-names/", UserNamesUpdateAPI.as_view(), name=input_fields.CHANGE_NAME),
-    path("change-password/", UserPasswordUpdateAPI.as_view(), name=input_fields.CHANGE_PASSWORD),
+    path("logout/", LogoutViewAPI.as_view(), name="logout"),
+    path("logoutall/", LogoutAllViewAPI.as_view(), name="logout-all"),
+    path("change-avatar/", UserAvatarAPI.as_view(), name="change-avatar"),
+    path("change-names/", UserNamesUpdateAPI.as_view(), name="change-names"),
+    path("change-password/", UserPasswordUpdateAPI.as_view(), name="change-password"),
     path("user-account/", UserAccountAPI.as_view(), name="user-account"),
     path("forgot-password/", SendForgotPasswordOTPAPIView.as_view(), name="forgot-password"),
     path("forgot-password/verify/<str:endpoint_token>/", PasswordResetOTPVerificationAPIView.as_view(), name="forgot-password-verify"),
     path("create-new-password/<str:endpoint_token>/", CreateNewPasswordTokenAPIView.as_view(), name="create-new-password"),
     path("whoami/", UserStatusAPI.as_view(), name="user-status"),
-
-
     path('<str:setting_name>/create/', UserSettingsToggleAPI.as_view(), name='user_settings_toggle'),
     path('lists/config/', UserSettingsListAPIView.as_view(), name='config-lists'),
     path('enable-2fa/', Enable2FAAPIView.as_view(), name='enable-2fa'),
