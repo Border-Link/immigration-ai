0aaa9d4576f4006b085d985b8d81392c0146a889
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-18 21:09:08 +0000

    user access, countries, states profiles doen

--- src/users_access/selectors/user_setting_selector.py
+++ src/users_access/selectors/user_setting_selector.py
@@ -1,7 +1,15 @@
 from users_access.models.user_settings import UserSetting
 
+
 class UserSettingSelector:
 
     @staticmethod
     def get_settings(user_id: str):
+        """Get user settings with user."""
         return UserSetting.objects.filter(user__id=user_id).first()
+
+    @staticmethod
+    def get_settings_by_user(user):
+        """Get user settings by user object."""
+        return UserSetting.objects.filter(user=user).first()
+
