f0fb17f2a6ce8875a4ed5e7f88301cbcd85213a6
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-18 21:09:09 +0000

    user access, countries, states profiles doen

--- src/users_access/views/users/names_update.py
+++ src/users_access/views/users/names_update.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from main_system.base.auth_api import AuthAPI
-from users_access.serializers.users.create_user_success import UserSerializer
-from users_access.services.user_service import UserService
+from users_access.services.user_profile_service import UserProfileService
+from users_access.serializers.user_profile.profile_serializer import UserProfileSerializer
 from users_access.serializers.users.names_update import NamesUpdateSerializer
 
 
@@ -12,14 +12,20 @@ class UserNamesUpdateAPI(AuthAPI):
         serializer = self.serializer_class(data=request.data, context={"request": request})
         serializer.is_valid(raise_exception=True)
 
-        service = UserService()
         first_name = serializer.validated_data.get('first_name')
         last_name = serializer.validated_data.get('last_name')
-        user = service.update_names(request.user, first_name, last_name)
+        profile = UserProfileService.update_names(request.user, first_name, last_name)
+
+        if not profile:
+            return self.api_response(
+                message="Error updating names.",
+                data=None,
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
         return self.api_response(
             message="User names updated successfully.",
-            data=UserSerializer(user).data,
+            data=UserProfileSerializer(profile).data,
             status_code=status.HTTP_200_OK
         )
 
