8420377c8b1174d4047688233256cc52bbeb4057
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-18 21:09:08 +0000

    user access, countries, states profiles doen

--- src/users_access/repositories/user_setting_repository.py
+++ src/users_access/repositories/user_setting_repository.py
@@ -3,20 +3,22 @@ from django.db import transaction
 from users_access.models.user_settings import UserSetting
 from helpers.totp import TOTPAuthenticator
 
+
 class UserSettingRepository:
 
     @staticmethod
-    def create_user_setting(user_id: str):
+    def create_user_setting(user):
+        """Create user settings for a user."""
         with transaction.atomic():
-            settings = UserSetting(user_id=user_id)
+            settings = UserSetting.objects.create(user=user)
             settings.full_clean()
             settings.save()
             return settings
 
     @staticmethod
-    def update_settings(user_id: str, settings_data: Dict):
+    def update_settings(settings, settings_data: Dict):
+        """Update settings fields."""
         with transaction.atomic():
-            settings = UserSetting.objects.get(user__id=user_id)
             for key, value in settings_data.items():
                 if hasattr(settings, key):
                     setattr(settings, key, value)
@@ -25,19 +27,29 @@ class UserSettingRepository:
             return settings
 
     @staticmethod
-    def delete_settings(user_id: str):
+    def delete_settings(settings):
+        """Delete user settings."""
         with transaction.atomic():
-            settings = UserSetting.objects.get(user__id=user_id)
             settings.delete()
             return True
 
     @staticmethod
-    def enable_2fa(user_id: str):
+    def enable_2fa(settings):
+        """Enable 2FA for user settings."""
         with transaction.atomic():
-            settings = UserSetting.objects.get(user__id=user_id)
             secret = TOTPAuthenticator.generate_totp()
             settings.two_factor_auth = True
             settings.totp_secret = secret
+            settings.full_clean()
+            settings.save()
+            return settings
+
+    @staticmethod
+    def disable_2fa(settings):
+        """Disable 2FA for user settings."""
+        with transaction.atomic():
+            settings.two_factor_auth = False
+            settings.totp_secret = None
             settings.full_clean()
             settings.save()
             return settings
