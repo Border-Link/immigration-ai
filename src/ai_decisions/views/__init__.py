a5e4e4c07f13eddf8a167a764c115f87127c3fb8
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-29 22:15:37 +0000

    adding most of the models, repositories, selectors and services

---
+++ src/ai_decisions/views/__init__.py
@@ -0,0 +1,12 @@
+from .eligibility_result.create import EligibilityResultCreateAPI
+from .eligibility_result.read import EligibilityResultListAPI, EligibilityResultDetailAPI
+from .eligibility_result.update_delete import EligibilityResultUpdateAPI, EligibilityResultDeleteAPI
+
+__all__ = [
+    'EligibilityResultCreateAPI',
+    'EligibilityResultListAPI',
+    'EligibilityResultDetailAPI',
+    'EligibilityResultUpdateAPI',
+    'EligibilityResultDeleteAPI',
+]
+
