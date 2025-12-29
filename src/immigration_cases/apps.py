91f01d8b41bf5029a0f5990e74ce3b7df2e41296
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-29 22:35:47 +0000

    adding signals and tasks

--- src/immigration_cases/apps.py
+++ src/immigration_cases/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ImmigrationCasesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "immigration_cases"
+
+    def ready(self):
+        from .signals import case_signals
