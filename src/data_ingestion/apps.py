4f1cba5b601b0c7433bc96bc511fcd6808214688
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-29 22:35:46 +0000

    adding signals and tasks

--- src/data_ingestion/apps.py
+++ src/data_ingestion/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class DataIngestionConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "data_ingestion"
+
+    def ready(self):
+        from .signals import rule_validation_signals
