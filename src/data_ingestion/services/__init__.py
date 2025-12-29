c5e2ae77a0b7d297f556adc5ba24b5c8a92c0a0e
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-29 22:15:38 +0000

    adding most of the models, repositories, selectors and services

--- src/data_ingestion/services/__init__.py
+++ src/data_ingestion/services/__init__.py
@@ -1,10 +1,20 @@
 from .ingestion_service import IngestionService
 from .data_source_service import DataSourceService
 from .rule_parsing_service import RuleParsingService
+from .source_document_service import SourceDocumentService
+from .document_version_service import DocumentVersionService
+from .document_diff_service import DocumentDiffService
+from .parsed_rule_service import ParsedRuleService
+from .rule_validation_task_service import RuleValidationTaskService
 
 __all__ = [
     'IngestionService',
     'DataSourceService',
     'RuleParsingService',
+    'SourceDocumentService',
+    'DocumentVersionService',
+    'DocumentDiffService',
+    'ParsedRuleService',
+    'RuleValidationTaskService',
 ]
 
