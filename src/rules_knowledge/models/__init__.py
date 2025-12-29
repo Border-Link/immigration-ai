c49324b28c3b8461ecd57f0014287bdb2604239d
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-29 22:15:40 +0000

    adding most of the models, repositories, selectors and services

---
+++ src/rules_knowledge/models/__init__.py
@@ -0,0 +1,14 @@
+from .document_type import DocumentType
+from .visa_type import VisaType
+from .visa_rule_version import VisaRuleVersion
+from .visa_requirement import VisaRequirement
+from .visa_document_requirement import VisaDocumentRequirement
+
+__all__ = [
+    'DocumentType',
+    'VisaType',
+    'VisaRuleVersion',
+    'VisaRequirement',
+    'VisaDocumentRequirement',
+]
+
