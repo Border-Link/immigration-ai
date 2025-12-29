b4b2fee2d2e805f6186487d6b6dd0777d54d1737
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-29 22:15:38 +0000

    adding most of the models, repositories, selectors and services

--- src/data_ingestion/views/__init__.py
+++ src/data_ingestion/views/__init__.py
@@ -2,11 +2,75 @@ from .data_source.create import DataSourceCreateAPI
 from .data_source.read import DataSourceListAPI, DataSourceDetailAPI
 from .data_source.update_delete import DataSourceUpdateAPI, DataSourceIngestionTriggerAPI
 
+from .source_document.read import (
+    SourceDocumentListAPI,
+    SourceDocumentDetailAPI,
+    SourceDocumentLatestAPI
+)
+
+from .document_version.read import (
+    DocumentVersionListAPI,
+    DocumentVersionDetailAPI,
+    DocumentVersionLatestAPI
+)
+
+from .document_diff.read import (
+    DocumentDiffListAPI,
+    DocumentDiffDetailAPI
+)
+
+from .parsed_rule.read import (
+    ParsedRuleListAPI,
+    ParsedRuleDetailAPI,
+    ParsedRulePendingAPI
+)
+from .parsed_rule.update_delete import (
+    ParsedRuleUpdateAPI,
+    ParsedRuleStatusUpdateAPI
+)
+
+from .rule_validation_task.read import (
+    RuleValidationTaskListAPI,
+    RuleValidationTaskDetailAPI,
+    RuleValidationTaskPendingAPI
+)
+from .rule_validation_task.update_delete import (
+    RuleValidationTaskUpdateAPI,
+    RuleValidationTaskAssignAPI,
+    RuleValidationTaskApproveAPI,
+    RuleValidationTaskRejectAPI
+)
+
 __all__ = [
+    # Data Source
     'DataSourceCreateAPI',
     'DataSourceListAPI',
     'DataSourceDetailAPI',
     'DataSourceUpdateAPI',
     'DataSourceIngestionTriggerAPI',
+    # Source Document
+    'SourceDocumentListAPI',
+    'SourceDocumentDetailAPI',
+    'SourceDocumentLatestAPI',
+    # Document Version
+    'DocumentVersionListAPI',
+    'DocumentVersionDetailAPI',
+    'DocumentVersionLatestAPI',
+    # Document Diff
+    'DocumentDiffListAPI',
+    'DocumentDiffDetailAPI',
+    # Parsed Rule
+    'ParsedRuleListAPI',
+    'ParsedRuleDetailAPI',
+    'ParsedRulePendingAPI',
+    'ParsedRuleUpdateAPI',
+    'ParsedRuleStatusUpdateAPI',
+    # Rule Validation Task
+    'RuleValidationTaskListAPI',
+    'RuleValidationTaskDetailAPI',
+    'RuleValidationTaskPendingAPI',
+    'RuleValidationTaskUpdateAPI',
+    'RuleValidationTaskAssignAPI',
+    'RuleValidationTaskApproveAPI',
+    'RuleValidationTaskRejectAPI',
 ]
-
