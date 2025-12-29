f208a8def1d970091a891125de56760429881006
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-29 22:15:39 +0000

    adding most of the models, repositories, selectors and services

--- src/main_system/permissions/__init__.py
+++ src/main_system/permissions/__init__.py
@@ -1,10 +1,12 @@
 from .is_superuser import IsSuperUser
 from .is_admin_or_staff import IsAdminOrStaff
+from .is_reviewer import IsReviewer
 from .factories.has_permission import HasPermission
 
 __all__ = [
     'IsSuperUser',
     'IsAdminOrStaff',
+    'IsReviewer',
     'HasPermission',
 ]
 
