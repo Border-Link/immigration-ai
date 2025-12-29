d304331e9054a657e01f3930b599d07323984fcb
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-29 22:35:47 +0000

    adding signals and tasks

--- src/rules_knowledge/apps.py
+++ src/rules_knowledge/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class RulesKnowledgeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "rules_knowledge"
+
+    def ready(self):
+        """Import signals when app is ready."""
+        import rules_knowledge.signals  # noqa
