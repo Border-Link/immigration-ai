b4256d39e1a1dbeb6582bb9c4566f46efc465d83
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2025-12-29 22:35:46 +0000

    adding signals and tasks

--- src/ai_decisions/apps.py
+++ src/ai_decisions/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AiDecisionsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "ai_decisions"
+
+    def ready(self):
+        from .signals import eligibility_result_signals
