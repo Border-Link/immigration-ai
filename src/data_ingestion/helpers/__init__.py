f7e248a2f3cfaeaa68c48509d81d6191f31f4c2b
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-09 00:08:36 +0000

    completed the ingestion system end to end

--- src/data_ingestion/helpers/__init__.py
+++ src/data_ingestion/helpers/__init__.py
@@ -1,5 +1,119 @@
 # Hash utilities have been moved to helpers/file_hashing
 # Import from there: from helpers.file_hashing import ContentHash
 
-__all__ = []
+from .prompts import (
+    get_rule_extraction_system_message,
+    get_rule_extraction_user_prompt,
+    get_jurisdiction_name
+)
+from .rate_limiter import (
+    TokenBucketRateLimiter,
+    get_rate_limiter,
+    DEFAULT_RATE_LIMIT_RPM,
+    DEFAULT_RATE_LIMIT_TPM,
+)
+from .requirement_codes import (
+    STANDARD_REQUIREMENT_CODES,
+    STANDARD_REQUIREMENT_CODES_SET,
+    is_standard_requirement_code,
+    get_requirement_code_category,
+    get_codes_by_category,
+    SALARY_CODES,
+    AGE_CODES,
+    EXPERIENCE_CODES,
+    SPONSOR_CODES,
+    LANGUAGE_CODES,
+    FINANCIAL_CODES,
+    NATIONALITY_CODES,
+    HEALTH_CODES,
+    CHARACTER_CODES,
+    EMPLOYMENT_CODES,
+    FAMILY_CODES,
+    DOCUMENT_CODES,
+    FEE_CODES,
+    PROCESSING_TIME_CODES,
+    OTHER_CODES,
+)
+from .rule_parsing_constants import (
+    DEFAULT_LLM_MODEL,
+    FALLBACK_LLM_MODEL,
+    LLM_TEMPERATURE,
+    LLM_MAX_TOKENS,
+    MAX_TEXT_LENGTH,
+    MIN_TEXT_LENGTH,
+    BASE_CONFIDENCE_SCORE,
+    NUMERIC_VALUE_MATCH_BONUS,
+    STANDARD_CODE_BONUS,
+    VALID_JSON_LOGIC_BONUS,
+    MAX_CONFIDENCE_SCORE,
+    DEFAULT_SLA_DAYS,
+    URGENT_SLA_DAYS,
+    HIGH_CONFIDENCE_THRESHOLD,
+    REQUIRED_RULE_FIELDS,
+    VALID_JSON_LOGIC_OPERATORS,
+)
+from .confidence_scorer import (
+    EnhancedConfidenceScorer,
+    compute_confidence_score as compute_enhanced_confidence_score,
+)
+from .parallel_processor import (
+    ParallelProcessor,
+    StreamingProcessor,
+)
+
+__all__ = [
+    # Prompts
+    'get_rule_extraction_system_message',
+    'get_rule_extraction_user_prompt',
+    'get_jurisdiction_name',
+    # Requirement Codes
+    'STANDARD_REQUIREMENT_CODES',
+    'STANDARD_REQUIREMENT_CODES_SET',
+    'is_standard_requirement_code',
+    'get_requirement_code_category',
+    'get_codes_by_category',
+    'SALARY_CODES',
+    'AGE_CODES',
+    'EXPERIENCE_CODES',
+    'SPONSOR_CODES',
+    'LANGUAGE_CODES',
+    'FINANCIAL_CODES',
+    'NATIONALITY_CODES',
+    'HEALTH_CODES',
+    'CHARACTER_CODES',
+    'EMPLOYMENT_CODES',
+    'FAMILY_CODES',
+    'DOCUMENT_CODES',
+    'FEE_CODES',
+    'PROCESSING_TIME_CODES',
+    'OTHER_CODES',
+    # Rule Parsing Constants
+    'DEFAULT_LLM_MODEL',
+    'FALLBACK_LLM_MODEL',
+    'LLM_TEMPERATURE',
+    'LLM_MAX_TOKENS',
+    'MAX_TEXT_LENGTH',
+    'MIN_TEXT_LENGTH',
+    'BASE_CONFIDENCE_SCORE',
+    'NUMERIC_VALUE_MATCH_BONUS',
+    'STANDARD_CODE_BONUS',
+    'VALID_JSON_LOGIC_BONUS',
+    'MAX_CONFIDENCE_SCORE',
+    'DEFAULT_SLA_DAYS',
+    'URGENT_SLA_DAYS',
+    'HIGH_CONFIDENCE_THRESHOLD',
+    'REQUIRED_RULE_FIELDS',
+    'VALID_JSON_LOGIC_OPERATORS',
+    # Rate Limiter
+    'TokenBucketRateLimiter',
+    'get_rate_limiter',
+    'DEFAULT_RATE_LIMIT_RPM',
+    'DEFAULT_RATE_LIMIT_TPM',
+    # Confidence Scorer
+    'EnhancedConfidenceScorer',
+    'compute_enhanced_confidence_score',
+    # Parallel Processor
+    'ParallelProcessor',
+    'StreamingProcessor',
+]
 
