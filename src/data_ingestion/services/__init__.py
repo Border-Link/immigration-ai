9e45612a7b179506a647c9477e178a3a45617c0f
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-09 00:26:11 +0000

    rule parsing optimization

--- src/data_ingestion/services/__init__.py
+++ src/data_ingestion/services/__init__.py
@@ -1,6 +1,6 @@
 from .ingestion_service import IngestionService
 from .data_source_service import DataSourceService
-from .rule_parsing_service import RuleParsingService
+from .rule_parsing.service import RuleParsingService
 from .source_document_service import SourceDocumentService
 from .document_version_service import DocumentVersionService
 from .document_diff_service import DocumentDiffService
