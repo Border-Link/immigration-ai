7efc4cc8262e7f64c6182823e0bbbff6913f087b
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-10 12:04:44 +0000

    completed the ai decision, data ingestion, users access, including the admin endpoints for all of them

--- src/users_access/serializers/notification/__init__.py
+++ src/users_access/serializers/notification/__init__.py
@@ -1,9 +1,15 @@
 from .create import NotificationMarkReadSerializer
 from .read import NotificationSerializer, NotificationListSerializer
+from .admin import (
+    NotificationCreateSerializer,
+    BulkNotificationCreateSerializer,
+)
 
 __all__ = [
     'NotificationMarkReadSerializer',
     'NotificationSerializer',
     'NotificationListSerializer',
+    'NotificationCreateSerializer',
+    'BulkNotificationCreateSerializer',
 ]
 
