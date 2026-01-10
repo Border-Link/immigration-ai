0e371af0c77626bb2bf9ee8c0e8efdc5b63fc0ba
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-10 12:23:03 +0000

    compliance completed

--- src/compliance/serializers/audit_log/__init__.py
+++ src/compliance/serializers/audit_log/__init__.py
@@ -1,7 +1,14 @@
 from .read import AuditLogSerializer, AuditLogListSerializer
+from .admin import (
+    AuditLogAdminListSerializer,
+    AuditLogAdminDetailSerializer,
+    BulkAuditLogOperationSerializer,
+)
 
 __all__ = [
     'AuditLogSerializer',
     'AuditLogListSerializer',
+    'AuditLogAdminListSerializer',
+    'AuditLogAdminDetailSerializer',
+    'BulkAuditLogOperationSerializer',
 ]
-
