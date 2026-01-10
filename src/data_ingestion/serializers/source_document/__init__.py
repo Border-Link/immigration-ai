12481b72b6533cddd166aee7f8ecc982907c4afe
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-10 12:04:42 +0000

    completed the ai decision, data ingestion, users access, including the admin endpoints for all of them

--- src/data_ingestion/serializers/source_document/__init__.py
+++ src/data_ingestion/serializers/source_document/__init__.py
@@ -1,7 +1,9 @@
 from .read import SourceDocumentSerializer, SourceDocumentListSerializer
+from .admin import BulkSourceDocumentOperationSerializer
 
 __all__ = [
     'SourceDocumentSerializer',
     'SourceDocumentListSerializer',
+    'BulkSourceDocumentOperationSerializer',
 ]
 
