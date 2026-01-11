5f10341c603890b8fffc2eb63c0320cc17a7952a
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-11 21:06:43 +0000

    optimization and payments completion

--- src/payments/repositories/__init__.py
+++ src/payments/repositories/__init__.py
@@ -1,6 +1,8 @@
 from .payment_repository import PaymentRepository
+from .payment_webhook_event_repository import PaymentWebhookEventRepository
 
 __all__ = [
     'PaymentRepository',
+    'PaymentWebhookEventRepository',
 ]
 
