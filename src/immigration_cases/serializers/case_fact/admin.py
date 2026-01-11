29df4cae0bcbb4329345799c6b1be983ff968d7d
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-11 19:08:48 +0000

    optimization updates

--- src/immigration_cases/serializers/case_fact/admin.py
+++ src/immigration_cases/serializers/case_fact/admin.py
@@ -4,36 +4,16 @@ Admin Serializers for CaseFact Management
 Serializers for admin case fact management operations.
 """
 from rest_framework import serializers
-from django.utils.dateparse import parse_datetime
+from main_system.serializers.admin.base import BaseAdminListQuerySerializer
 from immigration_cases.models.case_fact import CaseFact
 
 
-class CaseFactAdminListQuerySerializer(serializers.Serializer):
+class CaseFactAdminListQuerySerializer(BaseAdminListQuerySerializer):
     """Serializer for validating CaseFactAdminListAPI query parameters."""
     
     case_id = serializers.UUIDField(required=False, allow_null=True)
     fact_key = serializers.CharField(required=False, allow_null=True, max_length=255)
     source = serializers.ChoiceField(choices=CaseFact.SOURCE_CHOICES, required=False, allow_null=True)
-    date_from = serializers.DateTimeField(required=False, allow_null=True)
-    date_to = serializers.DateTimeField(required=False, allow_null=True)
-    
-    def validate(self, attrs):
-        """Validate date ranges."""
-        date_from = attrs.get('date_from')
-        date_to = attrs.get('date_to')
-        if date_from and date_to and date_to < date_from:
-            raise serializers.ValidationError({
-                'date_to': 'End date cannot be before start date.'
-            })
-        return attrs
-    
-    def to_internal_value(self, data):
-        """Parse date strings to datetime objects."""
-        if 'date_from' in data and isinstance(data['date_from'], str):
-            data['date_from'] = parse_datetime(data['date_from'])
-        if 'date_to' in data and isinstance(data['date_to'], str):
-            data['date_to'] = parse_datetime(data['date_to'])
-        return super().to_internal_value(data)
 
 
 class CaseFactAdminUpdateSerializer(serializers.ModelSerializer):
