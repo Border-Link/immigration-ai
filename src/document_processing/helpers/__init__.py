79f5adad109af9b0c0d3ecbb1e881bb770782612
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-11 15:02:11 +0000

    optimization and metric logic completed

--- src/document_processing/helpers/__init__.py
+++ src/document_processing/helpers/__init__.py
@@ -1,3 +1,19 @@
-from .status_transition_validator import StatusTransitionValidator
+from .metrics import (
+    track_processing_job_created,
+    track_processing_job_completed,
+    track_processing_job_retry,
+    track_processing_job_timeout,
+    update_processing_jobs_by_status,
+    update_processing_jobs_by_priority,
+    track_processing_history_entry
+)
 
-__all__ = ['StatusTransitionValidator']
+__all__ = [
+    'track_processing_job_created',
+    'track_processing_job_completed',
+    'track_processing_job_retry',
+    'track_processing_job_timeout',
+    'update_processing_jobs_by_status',
+    'update_processing_jobs_by_priority',
+    'track_processing_history_entry',
+]
