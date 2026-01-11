a6a1d156e4c2cc97cb57f09d25fdcb6fc28d1e15
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-11 19:08:49 +0000

    optimization updates

--- src/main_system/base/base_api.py
+++ src/main_system/base/base_api.py
@@ -1,7 +1,6 @@
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
-from helpers import fields as input_fields
 from rest_framework import serializers
 
 
@@ -10,7 +9,6 @@ class MyFallbackSerializer(serializers.Serializer):
 
 
 class BaseAPI(GenericAPIView):
-    field = input_fields
     service = None
 
     def get_serializer_class(self):
@@ -20,11 +18,12 @@ class BaseAPI(GenericAPIView):
 
     def api_response(self, *, message="", data=None, status_code=status.HTTP_200_OK, **kwargs):
         response = {
-            self.field.MESSAGE: message,
-            self.field.DATA: data,
+            "message": message,
+            "data": data,
             **kwargs
         }
         return Response(data=response, status=status_code)
+
 
 
 
