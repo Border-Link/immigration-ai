efe299723534e82aaf6f00f5fe0490a0bb8c7476
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-11 19:08:56 +0000

    optimization updates

--- src/users_access/serializers/users/add_avatar.py
+++ src/users_access/serializers/users/add_avatar.py
@@ -1,8 +1,5 @@
 from rest_framework import serializers
-from helpers import (
-    fields as input_fields,
-    image_processor as img_processor
-)
+from main_system.utils import image_processor as img_processor
 
 class UserAvatarSerializer(serializers.Serializer):
     avatar = serializers.ImageField(required=True)
@@ -10,7 +7,7 @@ class UserAvatarSerializer(serializers.Serializer):
     def validate_avatar(self, value):
        target_size_kb = 500
        image_quality = 85
-       image_format = input_fields.IMAGE_WEBP_FORMAT
+       image_format = "WEBP"
        return img_processor.ImageProcessor(value, target_size_kb, image_quality, image_format).process()
 
 
