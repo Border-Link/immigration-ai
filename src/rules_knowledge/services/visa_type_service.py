1a510e005c7f5843b21679142d566f8a87676e07
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-11 19:08:52 +0000

    optimization updates

--- src/rules_knowledge/services/visa_type_service.py
+++ src/rules_knowledge/services/visa_type_service.py
@@ -1,6 +1,6 @@
 import logging
 from typing import Optional
-from helpers.cache_utils import cache_result
+from main_system.utils.cache_utils import cache_result
 from rules_knowledge.models.visa_type import VisaType
 from rules_knowledge.repositories.visa_type_repository import VisaTypeRepository
 from rules_knowledge.selectors.visa_type_selector import VisaTypeSelector
