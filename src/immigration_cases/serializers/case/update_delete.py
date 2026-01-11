4eb5c9901f9d24a3083719424c50eb9970c194d6
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-11 08:02:44 +0000

    adding more optimization and caching system

--- src/immigration_cases/serializers/case/update_delete.py
+++ src/immigration_cases/serializers/case/update_delete.py
@@ -7,4 +7,6 @@ class CaseUpdateSerializer(serializers.Serializer):
     
     status = serializers.ChoiceField(choices=Case.STATUS_CHOICES, required=False)
     jurisdiction = serializers.ChoiceField(choices=Case.JURISDICTION_CHOICES, required=False)
+    version = serializers.IntegerField(required=True, help_text="Current version of the case for optimistic locking")
+    reason = serializers.CharField(required=False, allow_blank=True, max_length=500, help_text="Reason for the update")
 
