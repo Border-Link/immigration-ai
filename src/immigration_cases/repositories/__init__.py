9245bcb3d1c0fc0a4c7f0d3f71b87847622c999b
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-11 08:02:43 +0000

    adding more optimization and caching system

--- src/immigration_cases/repositories/__init__.py
+++ src/immigration_cases/repositories/__init__.py
@@ -1,8 +1,10 @@
 from .case_repository import CaseRepository
 from .case_fact_repository import CaseFactRepository
+from .case_status_history_repository import CaseStatusHistoryRepository
 
 __all__ = [
     'CaseRepository',
     'CaseFactRepository',
+    'CaseStatusHistoryRepository',
 ]
 
