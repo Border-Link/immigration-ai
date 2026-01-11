d27a7f5bd52f5742389e148fbd6bf2b4055e8550
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-11 19:08:51 +0000

    optimization updates

--- src/rules_knowledge/serializers/visa_type/admin.py
+++ src/rules_knowledge/serializers/visa_type/admin.py
@@ -4,42 +4,23 @@ Admin Serializers for VisaType Management
 Serializers for admin visa type management operations.
 """
 from rest_framework import serializers
-from django.utils.dateparse import parse_datetime
+from main_system.serializers.admin.base import (
+    BaseAdminListQuerySerializer,
+    ActivateSerializer as BaseActivateSerializer
+)
 
 
-class VisaTypeAdminListQuerySerializer(serializers.Serializer):
+class VisaTypeAdminListQuerySerializer(BaseAdminListQuerySerializer):
     """Serializer for validating VisaTypeAdminListAPI query parameters."""
     
     jurisdiction = serializers.CharField(required=False, allow_null=True, max_length=10)
     is_active = serializers.BooleanField(required=False, allow_null=True)
     code = serializers.CharField(required=False, allow_null=True, max_length=50)
-    date_from = serializers.DateTimeField(required=False, allow_null=True)
-    date_to = serializers.DateTimeField(required=False, allow_null=True)
-    page = serializers.IntegerField(required=False, min_value=1, default=1)
-    page_size = serializers.IntegerField(required=False, min_value=1, max_value=100, default=20)
-    
-    def validate(self, attrs):
-        """Validate date ranges."""
-        date_from = attrs.get('date_from')
-        date_to = attrs.get('date_to')
-        if date_from and date_to and date_to < date_from:
-            raise serializers.ValidationError({
-                'date_to': 'End date cannot be before start date.'
-            })
-        return attrs
-    
-    def to_internal_value(self, data):
-        """Parse date strings to datetime objects."""
-        if 'date_from' in data and isinstance(data['date_from'], str):
-            data['date_from'] = parse_datetime(data['date_from'])
-        if 'date_to' in data and isinstance(data['date_to'], str):
-            data['date_to'] = parse_datetime(data['date_to'])
-        return super().to_internal_value(data)
 
 
-class VisaTypeActivateSerializer(serializers.Serializer):
+class VisaTypeActivateSerializer(BaseActivateSerializer):
     """Serializer for activating/deactivating a visa type."""
-    is_active = serializers.BooleanField(required=True)
+    pass
 
 
 class BulkVisaTypeOperationSerializer(serializers.Serializer):
