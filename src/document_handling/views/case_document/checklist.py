2654c52186da64a0561d2a593ddda6bf6cd54d6c
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-11 19:08:44 +0000

    optimization updates

--- src/document_handling/views/case_document/checklist.py
+++ src/document_handling/views/case_document/checklist.py
@@ -5,9 +5,6 @@ from rest_framework import status
 from main_system.base.auth_api import AuthAPI
 from document_handling.services.document_checklist_service import DocumentChecklistService
 from document_handling.serializers.case_document.checklist import DocumentChecklistSerializer
-import logging
-
-logger = logging.getLogger('django')
 
 
 class DocumentChecklistAPI(AuthAPI):
@@ -15,20 +12,19 @@ class DocumentChecklistAPI(AuthAPI):
 
     def get(self, request, case_id: str):
         """Get document checklist for a case."""
-        try:
-            checklist = DocumentChecklistService.generate_checklist(case_id)
-            
-            serializer = DocumentChecklistSerializer(checklist)
-            
-            return self.api_response(
-                message="Document checklist retrieved successfully.",
-                data=serializer.data,
-                status_code=status.HTTP_200_OK
-            )
-        except Exception as e:
-            logger.error(f"Error retrieving document checklist: {e}", exc_info=True)
+        checklist = DocumentChecklistService.generate_checklist(case_id)
+        
+        if not checklist:
             return self.api_response(
                 message="Error retrieving document checklist.",
                 data=None,
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
+        
+        serializer = DocumentChecklistSerializer(checklist)
+        
+        return self.api_response(
+            message="Document checklist retrieved successfully.",
+            data=serializer.data,
+            status_code=status.HTTP_200_OK
+        )
