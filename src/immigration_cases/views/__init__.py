ac2103052fc016e493e7db1210161a2c3d5e25c9
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-11 08:02:45 +0000

    adding more optimization and caching system

--- src/immigration_cases/views/__init__.py
+++ src/immigration_cases/views/__init__.py
@@ -6,6 +6,23 @@ from .case_fact.create import CaseFactCreateAPI
 from .case_fact.read import CaseFactListAPI, CaseFactDetailAPI
 from .case_fact.update_delete import CaseFactUpdateAPI, CaseFactDeleteAPI
 
+# Admin views
+from .admin.case_admin import (
+    CaseAdminListAPI,
+    CaseAdminDetailAPI,
+    CaseAdminUpdateAPI,
+    CaseAdminDeleteAPI,
+    BulkCaseOperationAPI,
+)
+from .admin.case_fact_admin import (
+    CaseFactAdminListAPI,
+    CaseFactAdminDetailAPI,
+    CaseFactAdminUpdateAPI,
+    CaseFactAdminDeleteAPI,
+    BulkCaseFactOperationAPI,
+)
+from .admin.immigration_cases_analytics import ImmigrationCasesStatisticsAPI
+
 __all__ = [
     'CaseCreateAPI',
     'CaseListAPI',
@@ -19,5 +36,17 @@ __all__ = [
     'CaseFactDetailAPI',
     'CaseFactUpdateAPI',
     'CaseFactDeleteAPI',
+    # Admin
+    'CaseAdminListAPI',
+    'CaseAdminDetailAPI',
+    'CaseAdminUpdateAPI',
+    'CaseAdminDeleteAPI',
+    'BulkCaseOperationAPI',
+    'CaseFactAdminListAPI',
+    'CaseFactAdminDetailAPI',
+    'CaseFactAdminUpdateAPI',
+    'CaseFactAdminDeleteAPI',
+    'BulkCaseFactOperationAPI',
+    'ImmigrationCasesStatisticsAPI',
 ]
 
