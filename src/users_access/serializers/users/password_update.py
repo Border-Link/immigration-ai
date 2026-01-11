8d78d6aa33263bb77f970f2fd6337f83bbbb97e6
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-11 19:08:58 +0000

    optimization updates

--- src/users_access/serializers/users/password_update.py
+++ src/users_access/serializers/users/password_update.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.hashers import check_password
 from rest_framework import serializers
 from .password_validation import PasswordValidation
-from helpers import fields as input_fields
 
 
 class PasswordUpdateSerializer(serializers.Serializer):
@@ -9,9 +8,9 @@ class PasswordUpdateSerializer(serializers.Serializer):
     old_password = serializers.CharField(required=True, write_only=True)
 
     def validate_old_password(self, value):
-        user = self.context[input_fields.REQUEST].user
+        user = self.context["request"].user
         if not check_password(value, user.password):
-            raise serializers.ValidationError(input_fields.INVALID_OLD_PASSWORD)
+            raise serializers.ValidationError("Invalid old password")
 
 
     def validate_password(self, password):
