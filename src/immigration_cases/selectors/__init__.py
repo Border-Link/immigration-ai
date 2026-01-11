c2bcec5fc16020878769cfb81c9f07fe5b5bc655
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-11 08:02:44 +0000

    adding more optimization and caching system

--- src/immigration_cases/selectors/__init__.py
+++ src/immigration_cases/selectors/__init__.py
@@ -1,8 +1,10 @@
 from .case_selector import CaseSelector
 from .case_fact_selector import CaseFactSelector
+from .case_status_history_selector import CaseStatusHistorySelector
 
 __all__ = [
     'CaseSelector',
     'CaseFactSelector',
+    'CaseStatusHistorySelector',
 ]
 
