427950aecd4e7d74ed604ebfe760a1357cb163ab
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2026-01-15 09:28:36 +0000

    users access 284 tests passed

--- src/users_access/serializers/user_profile/__init__.py
+++ src/users_access/serializers/user_profile/__init__.py
@@ -2,9 +2,13 @@ from .profile_serializer import (
     UserProfileSerializer,
     UserProfileUpdateSerializer
 )
+from .avatar import UserAvatarSerializer
+from .names_update import NamesUpdateSerializer
 
 __all__ = [
     'UserProfileSerializer',
     'UserProfileUpdateSerializer',
+    'UserAvatarSerializer',
+    'NamesUpdateSerializer',
 ]
 
